a5b1ba60b148ecd804c6035d7b0ddf4d941502e8
Alighorashi95 <Alighorashi7@gmail.com>
2021-03-13 14:33:16 +0330

    simple calculator

--- Giraffe/App.py
+++ Giraffe/App.py
@@ -1,3 +1,4 @@
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-print("Hello " + name + "! You are " + age)
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+print(result)
