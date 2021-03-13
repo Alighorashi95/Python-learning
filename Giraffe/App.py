797f504c6140e184870064e317ec3763b314e0c8
Alighorashi95 <Alighorashi7@gmail.com>
2021-03-13 23:32:26 +0330

    Lists

--- Giraffe/App.py
+++ Giraffe/App.py
@@ -1,7 +1,3 @@
-color = input("Enter a color: ")
-plural_noun = input("Enter a Plural Noun: ")
-celebrity = input("Enter a celebrity: ")
 
-print("Roses are " + color)
-print(plural_noun + " are blue")
-print("I love " + celebrity)
+friends  = ["Kevin", "Karen", "Jim"]
+print(friends)
