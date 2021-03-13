e75bc6b6682c2c1bfcb1ee39e915ed728209e056
Alighorashi95 <Alighorashi7@gmail.com>
2021-03-13 23:47:49 +0330

    Lists Functions

--- Giraffe/App.py
+++ Giraffe/App.py
@@ -1,4 +1,4 @@
-
-friends  = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
-friends[1] = "Mike"
-print(friends[1])
+lucky_numbers = [4, 8, 15, 16, 23, 42]
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+friends.extend(lucky_numbers)
+print(friends)
