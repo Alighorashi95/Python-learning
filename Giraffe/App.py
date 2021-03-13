7b0f11587077cf6063b22173c4e05ac651e741a4
Alighorashi95 <Alighorashi7@gmail.com>
2021-03-13 12:00:43 +0330

    Variable

--- Giraffe/App.py
+++ Giraffe/App.py
@@ -1,4 +1,6 @@
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
+character_name = "Tom"
+character_age = "50"
+print("There once was a man named " + character_name + ", ")
+print("He was " + character_age + " years old. ")
+print("He really liked the name " + character_name + ", ")
+print("but did'nt like being " + character_age + ".")
