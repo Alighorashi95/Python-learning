8b09861439d6a5f6d863af1467f95a8493825465
Alighorashi95 <Alighorashi7@gmail.com>
2021-03-21 08:11:25 +0330

    Adding Information to a Function 2.2

--- Giraffe/App.py
+++ Giraffe/App.py
@@ -1,6 +1,6 @@
 
 def say_hello(name, age):
-    print("Hello " + name + ", you are " + age)
+    print("Hello " + name + ", you are " + str(age))
 
-say_hello("Mike", "35")
-say_hello("Steve", "70")
+say_hello("Mike", 35)
+say_hello("Steve", 70)
