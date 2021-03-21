02555e69f44188d7ba74dccb8ad5b89816d618a9
Alighorashi95 <Alighorashi7@gmail.com>
2021-03-21 09:04:33 +0330

    Else 2.2

--- Giraffe/App.py
+++ Giraffe/App.py
@@ -1,6 +1,6 @@
-is_male = True
-is_tall = True
+is_male = False
+is_tall = False
 if is_male or is_tall:
     print("You are a male or tall or both")
 else:
-    print("You are neither a male nor tall")
+    print("You are neither male nor tall")
