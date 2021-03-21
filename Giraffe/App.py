2e2509b810d2dd4e45aa4813f4a736cfc1a0219f
Alighorashi95 <Alighorashi7@gmail.com>
2021-03-21 08:19:14 +0330

    Return Statement

--- Giraffe/App.py
+++ Giraffe/App.py
@@ -1,6 +1,4 @@
 
-def say_hello(name, age):
-    print("Hello " + name + ", you are " + str(age))
-
-say_hello("Mike", 35)
-say_hello("Steve", 70)
+def cube(num):
+    return num*num*num
+print(cube(3))
