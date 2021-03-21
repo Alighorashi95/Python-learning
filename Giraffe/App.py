24c71be675db1d00ff55b0ff5db85c7a3464d58f
Alighorashi95 <Alighorashi7@gmail.com>
2021-03-21 09:13:52 +0330

    Elif

--- Giraffe/App.py
+++ Giraffe/App.py
@@ -1,6 +1,9 @@
 is_male = True
-is_tall = True
+is_tall = False
 if is_male and is_tall:
     print("You are a tall male")
+elif is_male and not(is_tall):
+    print("You are a short male")
 else:
     print("You are either not male or not tall or both")
+
