f5fc2471366923b15032a192632af20aab82feb8
Alighorashi95 <Alighorashi7@gmail.com>
2021-03-22 17:18:09 +0430

    Guessing Game

--- Giraffe/App.py
+++ Giraffe/App.py
@@ -1,6 +1,6 @@
 
-i = 1
-while i <= 10:
-    print(i)
-    i += 1
-print("Done with Loop")
+secret_word = "giraffe"
+guess = ""
+while guess != secret_word:
+    guess = input("Enter guess: ")
+print("You win!")
