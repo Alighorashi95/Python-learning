e1d1145e5985f6ca32090e5797edd8abe3e367d2
Alighorashi95 <Alighorashi7@gmail.com>
2021-03-23 23:10:15 +0430

    Comments

--- Giraffe/App.py
+++ Giraffe/App.py
@@ -1,13 +1,3 @@
 
-def translate(phrase):
-    translation = ""
-    for letter in phrase:
-        if letter.lower() in "aeiou":
-            if letter.isupper():
-                translation += "G"
-            else:
-                translation += "g"
-        else:
-            translation += letter
-    return translation
-print(translate(input("Enter a phrase: ")))
+# This program is cool.
+print("Comments are fun")
