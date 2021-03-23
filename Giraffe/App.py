717849ed7d9e83e97f8ad6379dd97768950348dd
Alighorashi95 <Alighorashi7@gmail.com>
2021-03-23 23:32:06 +0430

    Try Except 2 without error 1.2

--- Giraffe/App.py
+++ Giraffe/App.py
@@ -4,5 +4,7 @@ try:
     value = 10 / 0
     number = int(input("Enter a number: "))
     print(number)
+except ZeroDivisionError:
+    print("Devided by zero!")
 except:
     print("Invalid Input!")
