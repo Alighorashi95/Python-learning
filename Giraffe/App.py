c9fb8199ad54bca90abda3c144bff783bf5b7d38
Alighorashi95 <Alighorashi7@gmail.com>
2021-03-23 21:49:11 +0430

    Simple Translator 1.3

--- Giraffe/App.py
+++ Giraffe/App.py
@@ -3,7 +3,10 @@ def translate(phrase):
     translation = ""
     for letter in phrase:
         if letter.lower() in "aeiou":
-            translation += "g"
+            if letter.isupper():
+                translation += "G"
+            else:
+                translation += "g"
         else:
             translation += letter
     return translation
