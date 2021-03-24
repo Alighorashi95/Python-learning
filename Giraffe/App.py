0910ab5da2c50dcf8f13ea9308e210556249672a
Alighorashi95 <Alighorashi7@gmail.com>
2021-03-24 22:40:35 +0430

    Reading Files 2.6

--- Giraffe/App.py
+++ Giraffe/App.py
@@ -1,7 +1,5 @@
 
 
 employees_file = open("employees.txt", "r")
-print(employees_file.readline())
-print(employees_file.readline())
-print(employees_file.readline())
+print(employees_file.readlines())
 employees_file.close()
