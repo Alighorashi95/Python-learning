f9df7b946feaf85103dc10dc301ff94bae87b8a6
Alighorashi95 <Alighorashi7@gmail.com>
2021-04-21 12:40:54 +0430

    10

--- Intermediate/lists.py
+++ Intermediate/lists.py
@@ -1,12 +1,5 @@
 
 mylist = ["banana", "cherry", "apple"]
-mylist.append("lemon")
+item = mylist.pop()
+print(item)
 print(mylist)
-mylist.insert(1, "blueberry")
-print(mylist)
-
-mylist2 = list()
-print(mylist2)
-
-mylist3 = [5, True, "apple", "apple"]
-print(mylist3)
