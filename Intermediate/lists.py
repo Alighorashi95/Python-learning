edf3ec67883bf88d139fd0d757c3375eaa1dbc1e
Alighorashi95 <Alighorashi7@gmail.com>
2021-04-21 12:10:41 +0430

    1, 2

---
+++ Intermediate/lists.py
@@ -0,0 +1,6 @@
+
+mylist = ["banana", "cherry", "apple"]
+print(mylist)
+
+mylist2 = list()
+print(mylist2)
---
+++ ProjectEuler/Problem2.py
@@ -0,0 +1,11 @@
+
+fiblist = [1, 2]
+for i in fiblist[i] <= 4000000:
+    if i == 0 or i == 1:
+        continue
+    else:
+        fiblist.append(int(fiblist[i - 2]) + int(fiblist[i - 1]))
+        i += 1
+print(fibonacci)
+
+
