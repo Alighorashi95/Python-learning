48538007b59e53d32dc7745bbfacc2b05c38768b
Alighorashi95 <Alighorashi7@gmail.com>
2021-04-21 13:31:16 +0430

    24

--- Intermediate/lists.py
+++ Intermediate/lists.py
@@ -1,6 +1,5 @@
 
-list_org = ["banana", "cherry", "apple"]
-list_cpy = list_org[:]
-list_cpy.append("lemon")
-print(list_cpy)
-print(list_org)
+mylist = [1, 2, 3, 4, 5, 6]
+b = [i*i for i in mylist]
+print(mylist)
+print(b)
