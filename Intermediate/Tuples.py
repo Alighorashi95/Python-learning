af49b1a28be1933e3b5adcade89fd5ce14ba98ae
Alighorashi95 <Alighorashi7@gmail.com>
2021-04-23 15:51:19 +0430

    Tuples 7

--- Intermediate/Tuples.py
+++ Intermediate/Tuples.py
@@ -1,7 +1,5 @@
-
-my_tuple = (0, 1, 2, 3, 4)
-
-i1, *i2, i3 = my_tuple
-print(i1)
-print(i3)
-print(i2)
+import sys
+my_list = [0, 1, 2, "Hello", True]
+my_tuple = (0, 1, 2, "Hello", True)
+print(sys.getsizeof(my_list),  "bytes")
+print(sys.getsizeof(my_tuple),  "bytes")
