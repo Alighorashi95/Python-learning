ad244771ed5e07e600713bc7ea0940362b49d50e
Alighorashi95 <Alighorashi7@gmail.com>
2021-04-23 15:57:07 +0430

    Tuples 8

--- Intermediate/Tuples.py
+++ Intermediate/Tuples.py
@@ -1,5 +1,3 @@
-import sys
-my_list = [0, 1, 2, "Hello", True]
-my_tuple = (0, 1, 2, "Hello", True)
-print(sys.getsizeof(my_list),  "bytes")
-print(sys.getsizeof(my_tuple),  "bytes")
+import timeit
+print(timeit.timeit(stmt="[0, 1, 2, 3, 4, 5]", number=1000000))
+print(timeit.timeit(stmt="(0, 1, 2, 3, 4, 5)", number=1000000))
