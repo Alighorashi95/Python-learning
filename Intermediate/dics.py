9116973638437b21cc44311da8e0e6f445f5abdc
Alighorashi95 <Alighorashi7@gmail.com>
2021-04-24 18:32:16 +0430

    Dicts 3

--- Intermediate/dics.py
+++ Intermediate/dics.py
@@ -1,4 +1,2 @@
-my_dict = {"name":"Max", "age":28, "email":"max@xyz.com"}
-my_dict_2 = dict(name= "Mary", age= 27, city= "Boston")
-my_dict.update(my_dict_2)
+my_dict = {3: 9, 6: 36, 9: 81}
 print(my_dict)
