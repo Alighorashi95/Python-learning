1c9c7a54022da079ffd422d6fc0e662d7479b3a8
Alighorashi95 <Alighorashi7@gmail.com>
2021-04-24 18:19:05 +0430

    Dicts 1.16

--- Intermediate/dics.py
+++ Intermediate/dics.py
@@ -2,5 +2,8 @@ mydict = {"name":"Max", "age":28, "city":"New York"}
 print(mydict)
 
 mydict_cpy = mydict
-print(mydict_cpy)
 
+
+mydict_cpy["email"] = "max@xyz.com"
+print(mydict)
+print(mydict_cpy)
