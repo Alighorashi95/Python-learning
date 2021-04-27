cdb56e6683ba20dde58d9b3c69c6b484b4628a8f
Alighorashi95 <Alighorashi7@gmail.com>
2021-04-27 15:11:40 +0430

    Sets 4.4

--- Intermediate/sets.py
+++ Intermediate/sets.py
@@ -1,11 +1,6 @@
 
-odds = {1, 3, 5, 7, 9}
-evens = {0, 2, 4, 6, 8}
-primes = {2, 3, 5, 7}
+setA = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+setB = {1, 2, 3, 10, 11, 12}
 
-u = odds.union(evens)
-print(u)
-
-i = odds.intersection(primes)
-print(i)
-
+diff = setA.difference(setB)
+print(diff)
