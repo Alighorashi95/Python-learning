3de7cc3960f560534d3428325221a83d6b4b3ad5
Alighorashi95 <Alighorashi7@gmail.com>
2021-04-27 15:04:21 +0430

    Sets 4

--- Intermediate/sets.py
+++ Intermediate/sets.py
@@ -1,9 +1,7 @@
-myset = set()
 
-myset.add(1)
-myset.add(2)
-myset.add(3)
-for i in myset:
-    print(i)
-if 1 in myset:
-    print("yes")
+odds = {1, 3, 5, 7, 9}
+evens = {0, 2, 4, 6, 8}
+primes = {2, 3, 5, 7}
+
+u = odds.union(evens)
+print(u)
