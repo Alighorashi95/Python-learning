23b1060f5d8b7bf26808463236f06c1f61fff0f1
Alighorashi95 <Alighorashi7@gmail.com>
2021-04-28 10:50:50 +0430

    Strings 8.20

--- Intermediate/strings.py
+++ Intermediate/strings.py
@@ -1,12 +1,15 @@
-
-my_list = ["a"] * 6
-print(my_list)
+from timeit import default_timer as timer
+my_list = ["a"] * 1000000
 # bad
+start = timer()
 my_string = ""
 for i in my_list:
     my_string += i
-print(my_string)
+stop = timer()
+print(stop-start)
 
 #good
+start = timer()
 my_string = "".join(my_list)
-print(my_string)
+stop = timer()
+print(stop-start)
