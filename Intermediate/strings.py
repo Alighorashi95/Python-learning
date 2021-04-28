54fa6b1e1350399308bd10a7403a7930fbecaffb
Alighorashi95 <Alighorashi7@gmail.com>
2021-04-28 10:58:41 +0430

    Strings 'formatting the strings'

--- Intermediate/strings.py
+++ Intermediate/strings.py
@@ -1,15 +1,4 @@
-from timeit import default_timer as timer
-my_list = ["a"] * 1000000
-# bad
-start = timer()
-my_string = ""
-for i in my_list:
-    my_string += i
-stop = timer()
-print(stop-start)
 
-#good
-start = timer()
-my_string = "".join(my_list)
-stop = timer()
-print(stop-start)
+var = "Tom"
+my_string = "the variable is %s" % var
+print(my_string)
