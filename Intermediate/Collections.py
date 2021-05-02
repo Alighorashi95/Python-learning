394afa0d79fdb5ab224f7c0c50296899500b6728
Alighorashi95 <Alighorashi7@gmail.com>
2021-05-02 23:34:04 +0430

    collections OrderedDict 1.2

--- Intermediate/Collections.py
+++ Intermediate/Collections.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 ordered_dict = OrderedDict()
-ordered_dict["a"] = 1
+ordered_dict["d"] = 4
 ordered_dict["b"] = 2
 ordered_dict["c"] = 3
-ordered_dict["d"] = 4
+ordered_dict["a"] = 1
 print(ordered_dict)
