4bfda026bd4f1c75b21e8ed1e7eae11a009dbd2d
Alighorashi95 <Alighorashi7@gmail.com>
2021-05-03 00:28:37 +0430

    Itertools accumulate 1.3

--- Intermediate/itertools.py
+++ Intermediate/itertools.py
@@ -1,6 +1,6 @@
 from itertools import accumulate
 import operator
-a = [1, 2, 3, 4]
-acc = accumulate(a, func=operator.mul)
+a = [1, 2, 5, 3, 4]
+acc = accumulate(a, func= max)
 print(a)
 print(list(acc))
