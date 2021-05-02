982b2096f06ac84b9a6f6d3cd5e51360340aa9cd
Alighorashi95 <Alighorashi7@gmail.com>
2021-05-03 00:58:07 +0430

    Itertools groupby 2.2

--- Intermediate/itertools.py
+++ Intermediate/itertools.py
@@ -1,7 +1,7 @@
 from itertools import groupby
-def smaller_than_3(x):
-    return x < 3
+persons = [{"name" : "Tim", "age" : 25}, {"name" : "Dan", "age" : 25},
+           {"name" : "Lisa", "age" : 27}, {"name" : "Claire", "age" : 28}]
 a = [1, 2, 3, 4]
-group_obj = groupby(a, key= lambda x: x<3)
+group_obj = groupby(persons, key= lambda x: x["age"])
 for key, value in group_obj:
     print(key, list(value))
