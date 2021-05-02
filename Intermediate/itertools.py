522a8bc825e306abce93b1b7c4ae8ae8750c77af
Alighorashi95 <Alighorashi7@gmail.com>
2021-05-03 00:56:25 +0430

    Itertools groupby 2

--- Intermediate/itertools.py
+++ Intermediate/itertools.py
@@ -2,6 +2,6 @@ from itertools import groupby
 def smaller_than_3(x):
     return x < 3
 a = [1, 2, 3, 4]
-group_obj = groupby(a, key= smaller_than_3)
+group_obj = groupby(a, key= lambda x: x<3)
 for key, value in group_obj:
     print(key, list(value))
