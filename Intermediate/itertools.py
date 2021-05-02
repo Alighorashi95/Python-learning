998cf47519c48dad14abd8dd090ece6b0ef06704
Alighorashi95 <Alighorashi7@gmail.com>
2021-05-03 00:22:46 +0430

    Itertools accumulate

--- Intermediate/itertools.py
+++ Intermediate/itertools.py
@@ -1,6 +1,5 @@
-from itertools import combinations, combinations_with_replacement
+from itertools import accumulate
 a = [1, 2, 3, 4]
-comb = combinations(a, 2)
-print(list(comb))
-comb_wr = combinations_with_replacement(a, 2)
-print(list(comb_wr))
+acc = accumulate(a)
+print(a)
+print(list(acc))
