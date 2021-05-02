42fdac576c96067d46f83e3c238f039ae853d50f
Alighorashi95 <Alighorashi7@gmail.com>
2021-05-02 23:33:00 +0430

    collections OrderedDict

--- Intermediate/Collections.py
+++ Intermediate/Collections.py
@@ -1,4 +1,7 @@
-from collections import namedtuple
-point = namedtuple("point", "x,y")
-pt = point(1, -4)
-print(pt.x, pt.y)
+from collections import OrderedDict
+ordered_dict = OrderedDict()
+ordered_dict["a"] = 1
+ordered_dict["b"] = 2
+ordered_dict["c"] = 3
+ordered_dict["d"] = 4
+print(ordered_dict)
